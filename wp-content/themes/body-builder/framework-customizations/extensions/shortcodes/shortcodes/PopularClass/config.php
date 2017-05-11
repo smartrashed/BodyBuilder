3abdf0b31545173a85201e78b9d513395be5feda
rashedbitm <smartrashed@yahoo.com>
2017-05-11 15:27:50 +0600

    Done

--- wp-content/themes/body-builder/framework-customizations/extensions/shortcodes/shortcodes/PopularClass/config.php
+++ wp-content/themes/body-builder/framework-customizations/extensions/shortcodes/shortcodes/PopularClass/config.php
@@ -1,11 +1,9 @@
 <?php
-
-
+if (!defined('FW')) die('Forbidden');
 
 $cfg = array(
     'page_builder' => array(
         'title'         => esc_html__('Popular Class', 'body-builder'),
-        'description'   => esc_html__('This Shortcode will use Homepage Two Popular Class Section', 'body-builder'),
         'tab'           => esc_html__('Body Builder', 'body-builder'),
         'popup_size'    => 'medium' // can be large, medium or small
     )
